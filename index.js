428c4ba5197af8a24abf6dbc750481d0bae37568
Johan Lhour <johan.lhour@epitech.eu>
2018-12-06 15:59:43 +0100

    [FIX] import in app

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 
 import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
-import App from './app';
+import App from './app/index';
 
 AppRegistry.registerComponent(appName, () => App);
